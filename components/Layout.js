992804c83e81eb932ce3be762ddf8ba6f89aea19
Asane <yaradayo317@gmail.com>
2021-12-26 09:26:21 -0500

    created header, sidebar and footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import styles from "../styles/Layout.module.css";
+import Header from "./Header";
+import SideBar from "./SideBar";
+import Footer from "./Footer";
 
 export default function Layout({ title, keywords, description, children }) {
   return (
@@ -8,7 +11,10 @@ export default function Layout({ title, keywords, description, children }) {
         <title>{title}</title>
         <meta name="description" content={keywords} />
       </Head>
+      <Header />
+      <SideBar />
       <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 }
--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import Layout from "../../components/Layout";
+import Layout from "@Layout/components/Layout";
 
 export default function EventPage() {
   const router = useRouter();
