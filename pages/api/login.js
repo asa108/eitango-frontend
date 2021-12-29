9be1e33eeaa7d97042480959b754322b93b0d43a
Asane <yaradayo317@gmail.com>
2021-12-29 10:49:13 -0500

    added login function and got jwt

---
+++ pages/api/login.js
@@ -0,0 +1,32 @@
+import { API_URL } from "@/config/index";
+import cookie from "cookie";
+
+export default async (req, res) => {
+  if (req.method === "POST") {
+    const { identifier, password } = req.body;
+    const strapiRes = await fetch(`${API_URL}/auth/local`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        identifier,
+        password,
+      }),
+    });
+
+    const data = await strapiRes.json();
+    console.log(data.jwt);
+    if (strapiRes.ok) {
+      // set cookie
+      res.status(200).json({ user: data.user });
+    } else {
+      res
+        .status(data.statusCode)
+        .json({ message: data.message[0].messages[0].message });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).json({ massage: `Methods ${req.method} is not allowed` });
+  }
+};
